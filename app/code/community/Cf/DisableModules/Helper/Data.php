5a0ac87a7a1a30323ada0ce7a95c0536eab36722
Christoph F <cfrenes@animachi.de>
2012-03-12 04:27:00 -0700

    indent and typo changes

--- app/code/community/Cf/DisableModules/Helper/Data.php
+++ app/code/community/Cf/DisableModules/Helper/Data.php
@@ -2,9 +2,11 @@
 /**
  * Description of Data
  *
- * @author chris
+ * @category Cf
+ * @package Cf_DisableModules
+ * @author Christoph Frenes <cfrenes@animachi.de>
  */
 class Cf_DisableModules_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    
+
 }
